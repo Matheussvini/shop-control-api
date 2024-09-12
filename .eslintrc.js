f9cde16e816d5171b697c4da893129a1533ba16c
Matheus Vinicius <matheus.vinicius@elogica.info>
2024-09-12 17:43:02 -0300

    fix: eslint changes

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,27 +1,24 @@
 module.exports = {
-    parser: '@typescript-eslint/parser',
-    parserOptions: {
-      ecmaVersion: 2020,
-      sourceType: 'module',
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 2020,
+    sourceType: 'module',
+  },
+  extends: ['plugin:@typescript-eslint/recommended', 'plugin:import/recommended', 'plugin:prettier/recommended'],
+  rules: {
+    'import/no-unresolved': 'error',
+    'import/order': 'warn',
+    'import/no-named-as-default-member': 'off',
+    '@typescript-eslint/no-namespace': 'off',
+    'import/newline-after-import': ['error'],
+    'lines-between-class-members': ['error', 'always'],
+    '@typescript-eslint/no-unused-vars': ['warn'], // Ajuste para warning
+    'no-unused-vars': 'off', // Desative a regra padrão para evitar conflitos
+  },
+  settings: {
+    'import/parsers': {
+      '@typescript-eslint/parser': ['.ts', '.tsx'],
     },
-    extends: [
-      'plugin:@typescript-eslint/recommended',
-      'plugin:import/recommended',
-      'plugin:prettier/recommended', // Enables eslint-plugin-prettier and eslint-config-prettier. This will display prettier errors as ESLint errors. Make sure this is always the last configuration in the extends array.
-    ],
-    rules: {
-      'import/no-unresolved': 'error',
-      'import/order': 'warn',
-      'import/no-named-as-default-member': 'off',
-      '@typescript-eslint/no-namespace': 'off',
-      'import/newline-after-import': ['error'],
-      'lines-between-class-members': ['error', 'always'],
-    },
-    settings: {
-      'import/parsers': {
-        '@typescript-eslint/parser': ['.ts', '.tsx'],
-      },
-      'import/resolver': { typescript: {}, node: {} },
-    },
-  };
-  
+    'import/resolver': { typescript: {}, node: {} },
+  },
+};
