927286ab5f606c3bffa61e0a36def7c53968cad1
Matheus Vinicius <matheus.vinicius@elogica.info>
2024-09-13 11:03:53 -0300

    feat: validate query params middlware

--- src/middlewares/validation-middleware.ts
+++ src/middlewares/validation-middleware.ts
@@ -10,17 +10,19 @@ export function validateParams<T>(schema: ObjectSchema<T>): ValidationMiddleware
   return validate(schema, 'params');
 }
 
-function validate(schema: ObjectSchema, type: 'body' | 'params') {
+export function validateQuery<T>(schema: ObjectSchema<T>): ValidationMiddleware {
+  return validate(schema, 'query');
+}
+
+function validate(schema: ObjectSchema, type: 'body' | 'params' | 'query'): ValidationMiddleware {
   return (req: Request, res: Response, next: NextFunction) => {
     const { error } = schema.validate(req[type], {
       abortEarly: false,
+      convert: true,
     });
 
-    if (!error) {
-      next();
-    } else {
-      throw invalidDataError(error.details.map((d) => d.message));
-    }
+    if (error) throw invalidDataError(error.details.map((d) => d.message));
+    next();
   };
 }
 
